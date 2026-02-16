047872f8a93b4a7e71d3099207cae20ab530d1be
youssef_labnine <yousseflab20@gmail.com>
2026-02-16 16:51:45 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  base: '/yousseflab20-ui.github.io/'   // smiya dyal repo f GitHub
 })
